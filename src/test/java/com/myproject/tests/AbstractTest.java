80f0c7aa90084f28df90495fb5ed9bb76cab035f
Amit Hens <hens.is.here@gmail.com>
2025-04-23 23:47:58 +0530

    implemented basic grid - selenium.grid.enabled and remoteWebDriver

--- src/test/java/com/myproject/tests/AbstractTest.java
+++ src/test/java/com/myproject/tests/AbstractTest.java
@@ -1,11 +1,17 @@
 package com.myproject.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AbstractTest {
 
@@ -13,9 +19,31 @@ public class AbstractTest {
 
     @BeforeTest
     public void setDriver() {
-        WebDriverManager.edgedriver().setup();
-        this.driver = new EdgeDriver();
-        driver.manage().window().maximize();
+        try {
+            if (Boolean.getBoolean("selenium.grid.enabled")) {
+                this.driver = getRemoteDriver();
+            } else {
+                this.driver = getLocalDriver();
+                driver.manage().window().maximize();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities;
+        if(System.getProperty("browser").equalsIgnoreCase("chrome")) {
+            capabilities = new ChromeOptions();
+        } else {
+            capabilities = new FirefoxOptions();
+        }
+        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+    }
+
+    private WebDriver getLocalDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
     }
 
     @AfterTest
